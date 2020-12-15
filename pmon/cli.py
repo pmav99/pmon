97073669889310c0a0be182ad5314dcd1a3f2f67
Panos Mavrogiorgos <pmav99@gmail.com>
2020-12-15 11:29:37 +0200

    cli: Introduce different levels for RAM output verbosity

--- pmon/cli.py
+++ pmon/cli.py
@@ -1,47 +1,84 @@
+from __future__ import annotations
+
+import enum
 import signal
 import sys
 import time
 
+from typing import Any
+from typing import Dict
+from typing import Union
+
 import typer
 
-from .utils import get_proc, get_proc_data, HEADER, VERBOSE_HEADER
+from .utils import get_proc, get_proc_data, to_bytes
+
+
+TABS = 10
+
+
+class RAMVerbosity(str, enum.Enum):
+    short = "short"
+    default = "default"
+    verbose = "verbose"
+
+
+_HEADERS = {
+    RAMVerbosity.short: "uss,uss (%),cpu (%)",
+    RAMVerbosity.default: "vms,rss,pss,uss,shared,swap,rss (%),pss (%),uss (%),cpu (%)",
+    RAMVerbosity.verbose: "vms,rss,pss,uss,shared,text,lib,data,dirty,swap,rss (%),pss (%),uss (%),cpu (%)",
+}
+
+_KEYS = {
+    RAMVerbosity.short: "uss,uss_percent,cpu_percent".split(","),
+    RAMVerbosity.default: "vms,rss,pss,uss,shared,swap,rss_percent,pss_percent,uss_percent,cpu_percent".split(","),
+    RAMVerbosity.verbose: "vms,rss,pss,uss,shared,text,lib,data,dirty,swap,rss_percent,pss_percent,uss_percent,cpu_percent".split(
+        ","
+    ),  # fmt: off
+}
+
+
+def _get_header(ram: RAMVerbosity, color: bool) -> str:
+    header = "\t".join(_HEADERS[ram].split(",")).expandtabs(TABS)
+    header = typer.style(header, fg=typer.colors.GREEN, bold=True) if color else header
+    return header
+
+
+def _get_line(ram: RAMVerbosity, data: Dict[str, Union[int, float]]) -> str:
+    keys = _KEYS[ram]
+    values = [to_bytes(data[key]) for key in keys if not key.endswith("percent")]
+    values += ["%.2f" % data[key] for key in keys if key.endswith("percent")]
+    line = "\t".join(values).expandtabs(TABS)
+    return line
 
 
 app = typer.Typer()
-
-
-def _get_header(verbose: bool, color: bool) -> str:
-    header = VERBOSE_HEADER if verbose else HEADER
-    if color:
-        header = typer.style(header.expandtabs(10), fg=typer.colors.GREEN, bold=True)
-    else:
-        header = header.expandtabs(10)
-    return header
 
 
 @app.command(help="Process monitor for linux")
 def monitor(
     pid: int = typer.Argument(..., help="The pid of the process you want to monitor"),
     interval: float = typer.Option(0.5, min=0.1, help="The update interval in seconds"),
-    verbose: bool = typer.Option(False, help="Use verbose output"),
+    ram: RAMVerbosity = typer.Option(RAMVerbosity.default, help="Choose the verbosity of the RAM output"),
     color: bool = typer.Option(True, help="Use colored output"),
     show_header: int = typer.Option(20, min=0, help="Show the header line every N intervals. Use 0 to disable"),
-):
+) -> None:
     try:
         proc = get_proc(pid)
     except ValueError:
         typer.echo(f"The process does not exist: {pid}")
         raise typer.Exit(1)
-    header = _get_header(verbose=verbose, color=color)
+    header = _get_header(ram=ram, color=color)
     typer.echo(header)
     i = 0
     while True:
         try:
-            line = get_proc_data(proc, verbose=verbose)
+            data = get_proc_data(proc)
         except ValueError:
             typer.echo(f"The process no longer exists: {pid}")
             raise typer.Exit(1)
-        typer.echo(line.expandtabs(10))
+        line = _get_line(ram, data)
+        typer.echo(line)
         time.sleep(interval)
         if show_header:
             i += 1
@@ -51,7 +88,7 @@ def monitor(
 
 
 # handle Ctrl-C
-def sigint_handler(sig, frame):
+def sigint_handler(sig: Any, frame: Any) -> None:
     sys.exit(0)
 
 
--- pmon/utils.py
+++ pmon/utils.py
@@ -1,75 +1,38 @@
-from typing import Any
+from __future__ import annotations
+
 from typing import Dict
 from typing import List
-from typing import Tuple
+from typing import Union
 
-import psutil
+import psutil  # type: ignore
 
 
 __all__: List[str] = [
     "get_proc",
     "get_proc_data",
-    "HEADER",
-    "VERBOSE_HEADER",
 ]
 
 
-SYMBOLS = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-ATTRS = ("memory_full_info", "cpu_percent")
-HEADER = "\t".join("rss,vms,shared,pss,swap,uss,cpu (%),mem (%)".split(","))
-VERBOSE_HEADER = "\t".join("rss,vms,shared,text,lib,data,dirty,pss,swap,uss,cpu (%),mem (%)".split(","))
-PREFIX = {
-    'K': 1024,
-    'M': 1048576,
-    'G': 1073741824,
-    'T': 1099511627776,
-    'P': 1125899906842624,
-    'E': 1152921504606846976,
-    'Z': 1180591620717411303424,
-    'Y': 1208925819614629174706176,
+SYMBOLS: List[str] = ["K", "M", "G", "T", "P", "E", "Z", "Y"]
+TOTAL_MEMORY: int = psutil.virtual_memory().total
+PREFIX: Dict[str, int] = {
+    "K": 1024,
+    "M": 1048576,
+    "G": 1073741824,
+    "T": 1099511627776,
+    "P": 1125899906842624,
+    "E": 1152921504606846976,
+    "Z": 1180591620717411303424,
+    "Y": 1208925819614629174706176,
 }
 
 
-def to_bytes(n: int) -> str:
+def to_bytes(n: Union[int, float]) -> str:
     for symbol in reversed(SYMBOLS):
         if n >= PREFIX[symbol]:
             value = float(n) / PREFIX[symbol]
             return f"{value:.3f}{symbol}"
     return f"{n}B"
-
-
-def get_line(info: Dict[str, Any]) -> str:
-    memory_full_info = info["memory_full_info"]._asdict()
-    line = "\t".join([
-        to_bytes(memory_full_info["rss"]),
-        to_bytes(memory_full_info["vms"]),
-        to_bytes(memory_full_info["shared"]),
-        to_bytes(memory_full_info["pss"]),
-        to_bytes(memory_full_info["swap"]),
-        to_bytes(memory_full_info["uss"]),
-        f"{info['cpu_percent']:.2f}",
-        f"{info['memory_percent']:.2f}",
-    ])
-    return line
-
-
-def get_verbose_line(info: Dict[str, Any]) -> str:
-    memory_full_info = info["memory_full_info"]._asdict()
-    line = "\t".join([
-        to_bytes(memory_full_info["rss"]),
-        to_bytes(memory_full_info["vms"]),
-        to_bytes(memory_full_info["shared"]),
-        to_bytes(memory_full_info["text"]),
-        to_bytes(memory_full_info["lib"]),
-        to_bytes(memory_full_info["data"]),
-        to_bytes(memory_full_info["dirty"]),
-        to_bytes(memory_full_info["pss"]),
-        to_bytes(memory_full_info["swap"]),
-        to_bytes(memory_full_info["uss"]),
-        f"{info['cpu_percent']:.2f}",
-        f"{info['memory_percent']:.2f}",
-    ])
-    return line
 
 
 def get_proc(pid: int) -> psutil.Process:
@@ -81,16 +44,17 @@ def get_proc(pid: int) -> psutil.Process:
         return proc
 
 
-def get_proc_data(proc: psutil.Process, verbose: bool = False, attrs: Tuple[str] = ATTRS) -> str:
+def get_proc_data(proc: psutil.Process) -> Dict[str, Union[int, float]]:
+    data: Dict[str, Union[int, float]] = {}
     try:
         with proc.oneshot():
-            info = proc.as_dict(attrs=attrs)
-            info["memory_percent"] = proc.memory_percent("uss")
+            data.update(proc.memory_full_info()._asdict())
+            data["cpu_percent"] = proc.cpu_percent()
+            data["rss_percent"] = data["rss"] / TOTAL_MEMORY * 100
+            data["pss_percent"] = data["pss"] / TOTAL_MEMORY * 100
+            data["uss_percent"] = data["uss"] / TOTAL_MEMORY * 100
+            data["vms_percent"] = data["vms"] / TOTAL_MEMORY * 100
     except psutil.NoSuchProcess:
         raise ValueError(f"The process no longer exists: {proc.pid}")
     else:
-        if verbose:
-            line = get_verbose_line(info)
-        else:
-            line = get_line(info)
-        return line
+        return data
